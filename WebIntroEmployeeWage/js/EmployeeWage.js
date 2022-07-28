2dca5184d92b4eca1fea0fb69caf171acbdc98d3
shreegowri <gaganandh@gmail.com>
2022-07-28 09:49:44 +0530

    [shreegowri]Employee present or absent

---
+++ WebIntroEmployeeWage/js/EmployeeWage.js
@@ -0,0 +1,11 @@
+////////UC1-Check employee present or not////////////
+emp_Present=1;
+empCheck=(Math.random()*10)%2;
+if(empCheck==emp_Present)
+{
+    console.log("Employee present");
+}
+else
+{
+    console.log("Employee Absent");
+}
