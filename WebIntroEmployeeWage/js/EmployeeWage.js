8e317faa5e54898cefcfc545c05ff9703c9c7769
shreegowri <gaganandh@gmail.com>
2022-07-28 10:11:21 +0530

    [shreegowri]calculating employee wage with partime

--- WebIntroEmployeeWage/js/EmployeeWage.js
+++ WebIntroEmployeeWage/js/EmployeeWage.js
@@ -9,3 +9,25 @@ else
 {
     console.log("Employee Absent");
 }
+//////////////////UC2-to check parttime with full and absent//////////////////
+Emp_FullPresent=1;
+Emp_partTime=2;
+Emp_Rate_Per_Hr=20;
+empCheck=Math.floor(Math.random()*10)%3;
+if(empCheck==Emp_FullPresent)
+{
+    console.log("Employee present");
+    Emp_Hrs=8;
+}
+else if(empCheck==Emp_partTime)
+{
+    console.log("employee is in parttime");
+    Emp_Hrs=4;
+}
+else
+{
+    console.log("Employee Absent");
+    Emp_Hrs=0;
+}
+EmpWage=Emp_Rate_Per_Hr*Emp_Hrs;
+console.log("Employee wage:"+EmpWage);
