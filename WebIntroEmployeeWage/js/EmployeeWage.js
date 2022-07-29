ae64b726034badd02129f070ba5e38246c35e98f
shreegowri <gaganandh@gmail.com>
2022-07-29 09:56:27 +0530

    [shreegowri]refactor the code to implement the fuction

--- WebIntroEmployeeWage/js/EmployeeWage.js
+++ WebIntroEmployeeWage/js/EmployeeWage.js
@@ -12,23 +12,33 @@ else
 //////////////////UC2-to check parttime with full and absent//////////////////
 Emp_FullPresent=1;
 Emp_partTime=2;
+Emp_Full_Time_hrs=8;
+Emp_Part_Time_Hr=4;
 Emp_Rate_Per_Hr=20;
-switch(empCheck)
+Emp_Hrs=0;
+
+function getEmployeeWorkingHrs(empCheck)
 {
-    case Emp_FullPresent:
-        console.log("Employee in Full Time present");
-        Emp_Hrs=8;
-        break;
-    case Emp_FullPresent:
-        console.log("Employee partTime");
-        Emp_Hrs=4;
-        break;
-    default:
-        console.log("Employee Absent");
-        Emp_Hrs=0;
-        break;
+    switch(empCheck)
+    {
+        case Emp_FullPresent:
+            console.log("Employee in Full Time present");
+            return Emp_Full_Time_hrs;
+        case Emp_FullPresent:
+            console.log("Employee partTime");
+            return Emp_Part_Time_Hr;
+        default:
+            console.log("Employee Absent");
+            return 0;
+    }
 }
+Emp_Hrs=getEmployeeWorkingHrs(empCheck);
 EmpWage=Emp_Rate_Per_Hr*Emp_Hrs;
+console.log("UC-3-refactor the code to implement the fuction")
 console.log("Employee wage:"+EmpWage);
+
+///////////UC-3refactor the code to write a fuctiion to get work hours/////////////
+
+
 
 
