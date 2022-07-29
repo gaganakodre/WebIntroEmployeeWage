dda0135d91cdd29c5a7b4569d1404b041fb41852
shreegowri <gaganandh@gmail.com>
2022-07-29 10:37:38 +0530

    [shreegowri]calculating the monthly wage by assuming dyas as 20

--- WebIntroEmployeeWage/js/EmployeeWage.js
+++ WebIntroEmployeeWage/js/EmployeeWage.js
@@ -1,7 +1,7 @@
 ////////UC1-Check employee present or not////////////
 emp_Present=1;
-empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==emp_Present)
+empCheck1=Math.floor(Math.random()*10)%2;
+if(empCheck1==emp_Present)
 {
     console.log("Employee present");
 }
@@ -14,9 +14,11 @@ Emp_FullPresent=1;
 Emp_partTime=2;
 Emp_Full_Time_hrs=8;
 Emp_Part_Time_Hr=4;
+Num_of_Working_Days=20;
 Emp_Rate_Per_Hr=20;
 Emp_Hrs=0;
-
+empCheck=Math.floor(Math.random()*10)%3;
+///////////UC-3refactor the code to write a fuctiion to get work hours/////////////
 function getEmployeeWorkingHrs(empCheck)
 {
     switch(empCheck)
@@ -24,7 +26,7 @@ function getEmployeeWorkingHrs(empCheck)
         case Emp_FullPresent:
             console.log("Employee in Full Time present");
             return Emp_Full_Time_hrs;
-        case Emp_FullPresent:
+        case Emp_partTime:
             console.log("Employee partTime");
             return Emp_Part_Time_Hr;
         default:
@@ -32,12 +34,21 @@ function getEmployeeWorkingHrs(empCheck)
             return 0;
     }
 }
+
 Emp_Hrs=getEmployeeWorkingHrs(empCheck);
 EmpWage=Emp_Rate_Per_Hr*Emp_Hrs;
 console.log("UC-3-refactor the code to implement the fuction")
 console.log("Employee wage:"+EmpWage);
+////////////UC4-Calculate wage for a month assuming 20 working days////////
+for(let day=0;day<Num_of_Working_Days;day++)
+{
+    
+    Emp_Hrs+=getEmployeeWorkingHrs(empCheck);
+}
+EmpWage+=Emp_Hrs*Emp_Rate_Per_Hr;
+console.log("UC4-Calculate wage for a month assuming 20 working days so that The EmpWage For month="+EmpWage+ "And Emp hrous="+Emp_Hrs);
 
-///////////UC-3refactor the code to write a fuctiion to get work hours/////////////
+
 
 
 
