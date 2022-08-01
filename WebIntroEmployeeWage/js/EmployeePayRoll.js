f6a44db7d5572f03b5b7e05e2f27122d77460b1f
shreegowri <gaganandh@gmail.com>
2022-08-01 18:14:35 +0530

    [shreegowri]extended employee payroll class

--- WebIntroEmployeeWage/js/EmployeePayRoll.js
+++ WebIntroEmployeeWage/js/EmployeePayRoll.js
@@ -8,11 +8,21 @@ class EmployeePayRollData
     startDate;
 
     //constructor
-    constructor(id,name,salary)
+    //constructor(id,name,salary)
+    //{
+        //this.id=id;
+        //this.name=name;//uc12
+        //this.salary=salary;//uc12
+    //}
+    //parameterized constructor
+    //using spread operator
+    constructor(...params)
     {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
     //getter and setter
     //using getter and seeter we can acess name and we can apply logic here also
@@ -24,13 +34,18 @@ class EmployeePayRollData
     //method
     toString()
     {
-        return "id:"+this.id +",name: "+this.name +",salary" +this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return `Id: ${this.id} \tName: ${this.name} \tSalary: ${this.salary},\tGender: ${this.gender}, \tStart Date: ${empDate}`;
+        return "id:"+this.id +",name: "+this.name +",salary" +this.salary +",gender"+this.gender+ ",startDate"+this.startDate;
 
     }
 }
-let employePayrollData=new EmployeePayRollData(1, "janardhana",90876);
+let employePayrollData=new EmployeePayRollData(1, "janardhana",90876,);
 console.log(employePayrollData.toString());
 //if want to change the name and id
 employePayrollData.id=0;
 employePayrollData.name="jonny"
+console.log(employePayrollData.toString());
+employePayrollData=new EmployeePayRollData(1, "janardhana",90876,"M",new Date());
 console.log(employePayrollData.toString());
