38a4e73809ab488e08f60a2f0537749f7c972415
shreegowri <gaganandh@gmail.com>
2022-08-01 18:37:46 +0530

    [shreegowri]validating the name using regex pattern

--- WebIntroEmployeeWage/js/EmployeePayRoll.js
+++ WebIntroEmployeeWage/js/EmployeePayRoll.js
@@ -18,19 +18,45 @@ class EmployeePayRollData
     //using getter and seeter we can acess name and we can apply logic here also
     get name() {return this._name; }
     set name(name) {
-        console.log("To set the name: "+name)
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(name))//test is used to check pattren is mathing or not
+        {
+            this._name = name;
+        }
+        else throw 'Name is Incorrect!'; 
+        //console.log("To set the name: "+name)
         this._name=name;
     }
     //method
     toString()
     {
-        return "id:"+this.id +",name: "+this.name +",salary" +this.salary;
-
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id:"+this.id +",name: "+this.name +",\tsalary" +this.salary +", \tgender" +this.gender+",\tstartDate:"+this.startDate;
+        
     }
 }
-let employePayrollData=new EmployeePayRollData(1, "janardhana",90876);
+//UC13
+//Created obj for class using parameterized conbstructor
+let employePayrollData=new EmployeePayRollData(1, "Janardhana",90876,"M",new Date());
 console.log(employePayrollData.toString());
-//if want to change the name and id
-employePayrollData.id=0;
-employePayrollData.name="jonny"
-console.log(employePayrollData.toString());
+try{
+    employePayrollData.name="Jonny";
+    console.log(employePayrollData.toString());
+    
+}
+catch(e)
+{
+    console.error(e);
+}
+// //if want to change the name and id
+// employePayrollData.id=0;
+// employePayrollData.name="jonny"
+// console.log(employePayrollData.toString());
+// employePayrollData=new EmployeePayRollData(1, "janardhana",90876,"M",new Date());
+// console.log(employePayrollData.toString())
+
+
+
+
+
